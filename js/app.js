5401af21086aa77839453652a2a98b2916bc3cc4
Gustavo Miño <gus.desarrollo.web@gmail.com>
2022-04-26 03:19:17 -0300

    Creacion de la funcion computerPlay

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+//console.log("Hello World!");
+
+function computerPlay() {
+  const posiblesManos = ["piedra", "papel", "tijera"];
+  const random = Math.floor(Math.random() * 3);
+  return `${posiblesManos[random]}`;
+}
+
+console.log(computerPlay());
