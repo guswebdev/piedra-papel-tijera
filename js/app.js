02aa90a35def5a5949747f9d40f23c7e32b31bae
Gustavo Miño <gus.desarrollo.web@gmail.com>
2022-04-26 04:22:11 -0300

    Limpiando las funciones

--- js/app.js
+++ js/app.js
@@ -1,43 +1,66 @@
-//console.log("Hello World!");
-
 function computerPlay() {
   const posiblesManos = ["piedra", "papel", "tijera"];
   const random = Math.floor(Math.random() * 3);
   return `${posiblesManos[random]}`;
 }
 
-//console.log(computerPlay());
-
 function playRound(playerSelection, computerSelection) {
   // your code here!
   console.log(`player: ${playerSelection}`);
   console.log(`computer: ${computerSelection}`);
-  
+
   if (playerSelection === computerSelection) return `empate`;
 
   switch (computerSelection) {
     case "piedra":
       if (playerSelection === "tijera") {
-        return ("computer");
+        return "computer";
       } else {
-        return ("player");
+        return "player";
       }
     case "papel":
       if (playerSelection === "piedra") {
-        return ("computer");
+        return "computer";
       } else {
-        return ("player");
+        return "player";
       }
     case "tijera":
       if (playerSelection === "papel") {
-        return ("computer");
+        return "computer";
       } else {
-        return ("player");
+        return "player";
       }
   }
 }
 
-//const playerSelection = "piedra".toLowerCase();
-const playerSelection = computerPlay().toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let contador = 1;
+  let winsPlayer = 0;
+  let winsComputer = 0;
+
+  while (contador <= 5) {
+    console.log(`---------- Ronda ${contador} -------------`);
+    const playerSelection = prompt(
+      `Ingrese piedra, papel o tijera`
+    ).toLowerCase();
+    const computerSelection = computerPlay();
+    let ganador = playRound(playerSelection, computerSelection);
+    console.log(ganador);
+    if (ganador === "computer") {
+      winsComputer++;
+    } else if (ganador === "player") {
+      winsPlayer++;
+    }
+    contador++;
+  }
+
+  if (winsPlayer === winsComputer) {
+    return `Ha sido un empate`.toUpperCase();
+  } else if (winsPlayer > winsComputer) {
+    return `El ganador es Player`.toUpperCase();
+  } else {
+    return `El ganador es Computer`.toUpperCase();
+  }
+}
+
+console.log(game());
