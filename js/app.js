62485a35dc77a056a87a32ffa86fdf9d18390a2b
Gustavo Miño <gus.desarrollo.web@gmail.com>
2022-04-26 03:46:51 -0300

    Creacion de la funcion playRound

--- js/app.js
+++ js/app.js
@@ -6,4 +6,38 @@ function computerPlay() {
   return `${posiblesManos[random]}`;
 }
 
-console.log(computerPlay());
+//console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  console.log(`player: ${playerSelection}`);
+  console.log(`computer: ${computerSelection}`);
+  
+  if (playerSelection === computerSelection) return `empate`;
+
+  switch (computerSelection) {
+    case "piedra":
+      if (playerSelection === "tijera") {
+        return ("computer");
+      } else {
+        return ("player");
+      }
+    case "papel":
+      if (playerSelection === "piedra") {
+        return ("computer");
+      } else {
+        return ("player");
+      }
+    case "tijera":
+      if (playerSelection === "papel") {
+        return ("computer");
+      } else {
+        return ("player");
+      }
+  }
+}
+
+//const playerSelection = "piedra".toLowerCase();
+const playerSelection = computerPlay().toLowerCase();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
