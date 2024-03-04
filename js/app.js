551b7bbbeb7225d88fa69603839de9f8a4042461
Gustavo <gus.desarrollo.web@gmail.com>
2024-03-03 22:14:27 -0300

    Refactorizacion de piedra,papel o tijera

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 const d = document;
 let contJugador = 0;
 let contMaquina = 0;
+const $mensaje = d.querySelector(`[data-mensaje]`);
+const $ganador = d.querySelector(`[data-ganador]`);
+const $botones = d.querySelectorAll(`[data-mano]`);
+const $contJugador = d.querySelector(`[data-contador-jugador]`);
+const $contComputer = d.querySelector(`[data-contador-computer]`);
+
+
+const terminarJuego = (ganador) => {
+  $ganador.textContent = ganador;
+
+  $botones.forEach(el => {
+    el.setAttribute("disabled","true")
+  })
+
+}
 
 const convertirJugada = (jugada) => {
   let mano = "";
@@ -18,66 +33,78 @@ const convertirJugada = (jugada) => {
   return mano;
 }
 
-const tomarOpcionMaquina = () => {
-  let jugadaMaquina = Math.round(Math.random()*2);
-  return jugadaMaquina;
+const tomarOpcionMaquina = () => Math.round(Math.random()*2);
 
-}
-
-const jugarRonda = (manoJugador, manoMaquina) => {
+const jugarRonda = (manoJugador, manoMaquina,mensaje) => {
   
   
   if(manoJugador === manoMaquina){
-    console.log("empate, vamos de nuevo!");
+    
+    $mensaje.textContent = `empate, vamos de nuevo!`;
   } else {
     if(manoJugador == 0 && manoMaquina == 1){
-      console.log("Papel le gana a piedra");
+      
+      $mensaje.textContent = `Papel le gana a piedra`;
       contMaquina++;
     } else if(manoJugador == 0 && manoMaquina == 2) {
-      console.log("Piedra le gana a tijera");
+    
+      $mensaje.textContent = `Piedra le gana a tijera`;
       contJugador++;
     }
 
     if(manoJugador == 1 && manoMaquina == 0){
-      console.log("Papel le gana a piedra");
+      
+      $mensaje.textContent = `Papel le gana a piedra`;
       contJugador++;
     } else if(manoJugador == 1 && manoMaquina == 2) {
-      console.log("Tijera le gana a papel");
+      
+      $mensaje.textContent = `Tijera le gana a papel`;
       contMaquina++;
       
     }
 
     if(manoJugador == 2 && manoMaquina == 0){
-      console.log("Piedra le gana a tijera");
+      
+      $mensaje.textContent = `Piedra le gana a tijera`;
       contMaquina++;
     } else if(manoJugador == 2 && manoMaquina == 1) {
-      console.log("Tijera le gana a papel");
+      
+      $mensaje.textContent = `Tijera le gana a papel`;
       contJugador++;
+    }
+
+    $contJugador.textContent = `${contJugador}`;
+    $contComputer.textContent = `${contMaquina}`
+
+    if(contJugador == 5){
+      terminarJuego("Jugador");
+    } else if(contMaquina == 5){
+      terminarJuego("Computer")
     }
   }
   
 }
 
-const jugarJuego = () => {
-  let rondas = 0;
-  while (rondas < 5){
-    let manoJugador = prompt(`Ingrese 'piedra','papel' o 'tijera'`).toLowerCase();
-    jugarRonda(convertirJugada(manoJugador),tomarOpcionMaquina());
-    rondas++;
+const reset = () => {
+  $botones.forEach(el => {
+    el.removeAttribute("disabled");
+    contJugador=0;
+    contMaquina=0;
+    $contJugador.textContent = `0`;
+  $contComputer.textContent = `0`;
+  $ganador.textContent = `-`;
+  })
+}
+
+d.addEventListener("click",(e)=> {
+
+  if(e.target.dataset.mano == "piedra" || e.target.dataset.mano == "papel" || e.target.dataset.mano == "tijera"){
+    jugarRonda(convertirJugada(e.target.dataset.mano),tomarOpcionMaquina(),$mensaje)
   }
-}
 
-const mostrarGanador = () => {
-if(contJugador > contMaquina){
-  console.log(`El Jugador es el ganador`);
-} else if(contJugador < contMaquina) {
-  console.log(`La maquina es el ganador`);
-} else {
-  console.log(`Hubo un empate`);
-}
-
-}
+  if(e.target.dataset.reset == "reset"){
+    reset()
+  }
+})
 
 
-jugarJuego();
-mostrarGanador();
